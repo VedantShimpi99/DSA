96f2b44173bc8119bc806165a27629836dfcfeee
Vedant Shimpi <vedant.shimpi99@gmail.com>
2025-06-07 23:28:30 +0530

    initial commit

---
+++ pattern2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    int n, i, j;
+    cin >> n ;
+
+    for(i = n; i>=0; i--)
+    {
+        for(j=0; j<i+1; j++)
+        {
+
+            cout << " * ";
+
+        }
+
+        cout << endl;
+
+    }
+
+    return 0;
+}
